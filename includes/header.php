081201277a02516c74a8aa0e3da6ec13e2848d0e
GH-Prince-Laxamana <prince.laxamana116@gmail.com>
2026-01-16 03:14:50 +0800

    Final revisions

--- includes/header.php
+++ includes/header.php
@@ -11,7 +11,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="css/styles.css?a=1" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
-  <title>Bank Object</title>
+  <title>Asenso</title>
 </head>
 
 <body>
